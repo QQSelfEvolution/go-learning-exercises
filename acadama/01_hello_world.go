ac25e80fb35557285ac8e37e1264b230041dbc37
QQ-AGENT <247658430@qq.com>
2026-05-09 14:37:13 +0800

    Add 阿代码 Day1 01_hello_world.go

---
+++ acadama/01_hello_world.go
@@ -0,0 +1,18 @@
+// 阿代码 Day1练习 #1: Hello World
+// Go语言基础 - 第一个程序
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("=== 阿代码 Go语言学习 Day1 ===")
+    fmt.Println("Hello World! 我是阿代码!")
+    fmt.Println("开始学习Go语言!")
+    
+    // 基础输出
+    name := "阿代码"
+    age := 25
+    fmt.Printf("我叫%s，今年%d岁\n", name, age)
+    
+    fmt.Println("=== Day1练习1完成 ===")
+}
